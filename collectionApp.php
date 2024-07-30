24b172c011e241ae96765e94a35010ce184130a3
Emma Ferrari <e.ferrari@live.co.uk>
2024-07-30 15:21:18 +0100

    made responsive and presentable

--- collectionApp.php
+++ collectionApp.php
@@ -11,35 +11,31 @@
 <h1>Camera Collection App</h1>
 
 <div class="mainContainer">
-<?php
-$db = new PDO('mysql:host=db; dbname=cameras', 'root', 'password');
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    <?php
+    $db = new PDO('mysql:host=db; dbname=cameras', 'root', 'password');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-$query = $db->prepare("SELECT `brandName`, `model`, `type`, `megapixels`, `weight(g)`, `images` FROM `cameras`");
-$query->execute();
-$results = $query->fetchAll();
+    $query = $db->prepare("SELECT `brandName`, `model`, `type`, `megapixels`, `weight(g)`, `images` FROM `cameras`");
+    $query->execute();
+    $results = $query->fetchAll();
 
 
-foreach ($results as $result)
-{
-    echo '<div class="imageAndTextContainer">';
-    echo '<div class = "imageContainer">';
-    echo '<img class="imagesClass" src="' . $result['images'] . '" alt="camera">';
-    echo '<br><br>';
-    echo '</div>';
+    foreach ($results as $result) {
+        echo '<div class="imageAndTextContainer">';
+        echo '<div class = "imageContainer">';
+        echo '<img class="imagesClass" src="' . $result['images'] . '" alt="camera">';
+        echo '<br><br>';
+        echo '</div>';
 
-    echo '<div class = "textContainer">';
-    echo $result['brandName'] ." <br>" . $result['model'] . " <br>" . $result['type'] . " <br>" .
-                    $result['megapixels'] . " megapixels<br>" . $result['weight(g)'] . "g<br><br>";
-    echo '</div>';
-    echo '</div>';
-}
-?>
+        echo '<div class = "textContainer">';
+        echo $result['brandName'] . " <br>" . $result['model'] . " <br>" . $result['type'] . " <br>" .
+            $result['megapixels'] . " megapixels<br>" . $result['weight(g)'] . "g<br><br>";
+        echo '</div>';
+        echo '</div>';
+    }
+    ?>
 </div>
 </body>
 
-<footer>
-
-</footer>
 </html>
