61a64cbe709e350b806357a3d86a22d5f85b3fb4
Emma Ferrari <e.ferrari@live.co.uk>
2024-07-31 13:54:32 +0100

    form works a bit better

--- index.php
+++ index.php
@@ -14,6 +14,46 @@
     <?php
     require_once 'dbCameras.php';
     global $db;
+
+
+    $brandName = $model = $type = $megapixels = $weight = $images = '';
+
+    if ($_SERVER ['REQUEST_METHOD'] == 'POST') {
+        $brandName = test_input($_POST['brandName']);
+        $model = test_input($_POST['model']);
+        $type = test_input($_POST['type']);
+        $megapixels = test_input($_POST['megapixels']);
+        $weight = test_input($_POST['weight(g)']);
+        //$images = test_input($_POST['images']);
+
+        $query = $db->prepare("INSERT INTO cameras (`brandName`, `model`, `type`, `megapixels`, `weight(g)`, `images`) VALUES (:brandName, :model, :type, :megapixels, :weight, :images)");
+        $query->bindParam(':brandName', $brandName);
+        $query->bindParam(':model', $model);
+        $query->bindParam(':type', $type);
+        $query->bindParam(':megapixels', $megapixels);
+        $query->bindParam(':weight', $weight);
+        $query->bindParam(':images', $images);
+
+        $query->execute();
+
+    }
+
+    function test_input($data)
+    {
+        if ($data)
+        {
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
+        }
+        else
+        {
+            return null;
+        }
+
+    }
+
     $query = $db->prepare("SELECT `brandName`, `model`, `type`, `megapixels`, `weight(g)`, `images` FROM `cameras`");
     $query->execute();
     $results = $query->fetchAll();
@@ -33,29 +73,7 @@
     }
     ?>
 </div>
-
 <div>
-    <?php
-    $brandName = $model = $type = $megapixels = $weight = $images = '';
-
-    if ($_SERVER ['REQUEST_METHOD'] == 'POST')
-    {
-        $brandName = test_input($_POST['brandName']);
-        $model = test_input($_POST['model']);
-        $type = test_input ($_POST['type']);
-        $megapixels = test_input ($_POST['megapixels']);
-        $weight = test_input ($_POST['weight(g)']);
-    }
-    function test_input($data)
-    {
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
-    }
-
-    ?>
-
     <h2>Add to the collection: </h2>
     <form method="POST" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>">
         Brand Name: <input type="text" name="brandName"><br>
@@ -63,21 +81,9 @@
         Type: <input type="text" name="type"><br>
         Megapixels: <input type="text" name="megapixels"><br>
         Weight(g): <input type="text" name="weight(g)"><br>
+        Image: <input type="text" name="image"><br>
         <input type="submit">
     </form>
-
-    <?php
-    echo $brandName;
-    echo '<br>';
-    echo $model;
-    echo '<br>';
-    echo $type;
-    echo '<br>';
-    echo $megapixels;
-    echo '<br>';
-    echo $weight;
-    ?>
-
 
 </div>
 </body>
