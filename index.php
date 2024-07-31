154d8ad6a8341db2ee91c154de10efbed5c68ad1
Emma Ferrari <e.ferrari@live.co.uk>
2024-07-31 10:39:19 +0100

    added form

--- index.php
+++ index.php
@@ -33,6 +33,53 @@
     }
     ?>
 </div>
+
+<div>
+    <?php
+    $brandName = $model = $type = $megapixels = $weight = $images = '';
+
+    if ($_SERVER ['REQUEST_METHOD'] == 'POST')
+    {
+        $brandName = test_input($_POST['brandName']);
+        $model = test_input($_POST['model']);
+        $type = test_input ($_POST['type']);
+        $megapixels = test_input ($_POST['megapixels']);
+        $weight = test_input ($_POST['weight(g)']);
+    }
+    function test_input($data)
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+
+    ?>
+
+    <h2>Add to the collection: </h2>
+    <form method="POST" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>">
+        Brand Name: <input type="text" name="brandName"><br>
+        Model: <input type="text" name="model"><br>
+        Type: <input type="text" name="type"><br>
+        Megapixels: <input type="text" name="megapixels"><br>
+        Weight(g): <input type="text" name="weight(g)"><br>
+        <input type="submit">
+    </form>
+
+    <?php
+    echo $brandName;
+    echo '<br>';
+    echo $model;
+    echo '<br>';
+    echo $type;
+    echo '<br>';
+    echo $megapixels;
+    echo '<br>';
+    echo $weight;
+    ?>
+
+
+</div>
 </body>
 
 </html>
